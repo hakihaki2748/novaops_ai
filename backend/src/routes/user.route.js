e3d2f73b0cf55609e0c75b0087e1fdd5beb80094
Haki <qu.haky@gmail.com>
2026-08-16 19:10:13 +0700

    update user route dan validasi create user schema

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -8,14 +8,14 @@ import userController from "../controllers/user.controller.js";
 import validate from "../validations/validate.js"
 import updateStatusSchema from "../validations/schemas/userUpdateStatus.schema.js"
 import updateRoleSchema from "../validations/schemas/userUpdateRole.schema.js"
+import createUserSchema from "../validate/schemas/createUser.schema.js"
 
-
-router.get("/", authMiddleware, roleMiddleware("owner", "manager", "admin"), userController.getUsers);
+router.get("/", authMiddleware, roleMiddleware("owner", "manager", "admin", "user"), userController.getUsers);
 router.get("/:id", authMiddleware, roleMiddleware("owner", "manager", "admin", "user"), userController.findUserById);
-router.post("/", authMiddleware, roleMiddleware("owner", "admin"), userController.createUser);
-router.delete("/:id", authMiddleware, roleMiddleware("owner"), userController.softDelete);
-router.patch("/:id/status",authMiddleware, roleMiddleware("owner", "manager"), validate(updateStatusSchema), userController.updateStatus);
-router.patch("/:id/role",authMiddleware, roleMiddleware("owner", "manager", "admin"), validate(updateRoleSchema), userController.updateRole);
+router.post("/", authMiddleware, roleMiddleware("owner", "admin"),validate(createUserSchema), userController.createUser);
+router.delete("/:id", authMiddleware, roleMiddleware("owner", "admin"), userController.softDelete);
+router.patch("/:id/status",authMiddleware, roleMiddleware("owner", "admin"), validate(updateStatusSchema), userController.updateStatus);
+router.patch("/:id/role",authMiddleware, roleMiddleware("owner", "admin"), validate(updateRoleSchema), userController.updateRole);
 
 
 export default router;
--- backend/src/validations/schemas/createUser.schema.js
+++ backend/src/validations/schemas/createUser.schema.js
@@ -1,24 +1,29 @@
+import { z } from "zod";
+
 export const createUserSchema = z.object({
     name: z
         .string()
         .trim()
         .min(1, "Name is required")
         .max(100, "Name is too long"),
+    
+    phone: z
+      .string()
+      .trim()
+      .min(10, "Phone number is too short")
+      .max(20, "Phone number is too long")
+      .regex(/^[0-9+]+$/, "Invalid phone number"),
 
     email: z
-        .string()
-        .trim()
-        .email("Invalid email address"),
+      .string()
+      .trim()
+      .email("Invalid email address"),
 
-    status: z.enum([
-        "active",
-        "inactive",
-        "suspended"
-    ]),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .max(100, "Password is too long"),
 
-    age: z
-        .number()
-        .int()
-        .min(18)
-        .max(100)
+    role: z.enum(["admin", "manager", "user"])
+
 }).strict();
