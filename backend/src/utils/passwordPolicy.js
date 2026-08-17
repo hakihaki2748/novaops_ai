93fcb7f24f9b1e4aa4a17aa05153e5db83e6acf3
Haki <qu.haky@gmail.com>
2026-08-17 21:21:56 +0700

    memperbaiki kondisi validatePasswordPolicy

--- backend/src/utils/passwordPolicy.js
+++ backend/src/utils/passwordPolicy.js
@@ -1,55 +1,48 @@
-export function validatePassword (password){
-    //role
+export function validatePasswordPolicy (password){
+
+    let valid = ""
+    const errors = []
     // minimum 12
     if(typeof password !== "string"){
-        return {
-            valid: false,
-            message: "Password harus String"
-        }
+        
+            valid = false,
+            errors.push("Password harus String")
+    
     };
 
     // maximum 100
-    if(password.length < 12 && password.length > 100){
-        return {
-            valid: false,
-            message: "Carachter Antara 12 dan 100 Digit"
-        }
+    if(password.length < 12 || password.length > 100){
+        valid = false,
+        errors.push("Karakter kurang dari 12 atau lebih dari 100 digit")
     };
     
     // uppercase
     if(!/[A-Z]/.test(password)){
-        return {
-            valid: false,
-            message: "Minimal ada 1 Huruf Kapital"
-        }
+            valid= false,
+            errors.push("Minimal ada 1 Huruf Kapital")
+    
     };
 
     // lowercase
     if (!/[a-z]/.test(password)){
-        return {
-            valid: false,
-            message: "Minimal ada 1 Huruf Kecil"
-        }
+            valid = false,
+            error.push("Minimal ada 1 Huruf Kecil")
     };
 
     // number
     if(!/[0-9]/.test(password)){
-        return {
-            valid: false,
-            message: "Minimal ada 1 Angka"
-        }
+        valid = false
+        errors.push("Minimal ada 1 Angka")
     };
     
     // symbol
     if(!/[^A-Za-z0-9]/.test(password)){
-        return {
-            valid: false,
-            message: "Minimal ada 1 Symbol"
-        }
+        valid = false
+        error.push("Minimal ada 1 Symbol")
     }
 
     return {
-        valid: true,
-        message: null
+        valid: true ,
+        errors
     }
 }
