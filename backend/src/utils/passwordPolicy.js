c8d6b5c318589ba758f7518c4c5b1121cfbc56c0
Haki <qu.haky@gmail.com>
2026-08-17 20:53:10 +0700

    memperbaiki validatePassword

--- backend/src/utils/passwordPolicy.js
+++ backend/src/utils/passwordPolicy.js
@@ -1,12 +1,55 @@
-import { z } from "zod";
+export function validatePassword (password){
+    //role
+    // minimum 12
+    if(typeof password !== "string"){
+        return {
+            valid: false,
+            message: "Password harus String"
+        }
+    };
 
-const passwordSchema = z
-  .string()
-  .min(12, "Password must be at least 12 characters")
-  .max(100, "Password must not exceed 100 characters")
-  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-  .regex(/[0-9]/, "Password must contain at least one number")
-  .regex(/[^A-Za-z0-9]/, "Password must contain at least one symbol");
+    // maximum 100
+    if(password.length < 12 && password.length > 100){
+        return {
+            valid: false,
+            message: "Carachter Antara 12 dan 100 Digit"
+        }
+    };
+    
+    // uppercase
+    if(!/[A-Z]/.test(password)){
+        return {
+            valid: false,
+            message: "Minimal ada 1 Huruf Kapital"
+        }
+    };
 
-export default passwordSchema
+    // lowercase
+    if (!/[a-z]/.test(password)){
+        return {
+            valid: false,
+            message: "Minimal ada 1 Huruf Kecil"
+        }
+    };
+
+    // number
+    if(!/[0-9]/.test(password)){
+        return {
+            valid: false,
+            message: "Minimal ada 1 Angka"
+        }
+    };
+    
+    // symbol
+    if(!/[^A-Za-z0-9]/.test(password)){
+        return {
+            valid: false,
+            message: "Minimal ada 1 Symbol"
+        }
+    }
+
+    return {
+        valid: true,
+        message: null
+    }
+}
