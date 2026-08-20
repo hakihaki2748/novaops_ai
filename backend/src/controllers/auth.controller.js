dd5335ff2e4789e5064c39112f1ad195a6d96967
Haki <qu.haky@gmail.com>
2026-08-20 10:26:18 +0700

    sudah diperbaiki authService

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import authService from "../services/auth.service.js"
 import apiResponse from "../utils/apiResponse.js";
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
     try {
         const result = await authService.login(req.body);
         res.status(200).json(
