58d0905b295ada7daba9db21b97590d99c40a8d4
Haki <qu.haky@gmail.com>
2026-08-24 10:39:46 +0700

    perbaikan userManagement

--- frontend/src/views/UserManagement.vue
+++ frontend/src/views/UserManagement.vue
@@ -3,56 +3,24 @@ import UserSearch from '@/components/userManagement/UserSearch.vue';
 import UserTable from '@/components/userManagement/UserTable.vue';
 import UserPagination from '@/components/userManagement/UserPagination.vue';
 import { useUserStore } from '@/stores/user.store';
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 
-const users = ref([])
-const user = ref([])
-const page = ref(1)
-const limit = ref(10)
-const search = ref("")
-const error = ref(null)
+const userStore = useUserStore();
 
-const loadUsers = async(params) => {
-    try{
-        const res = await useUserStore.loadUsers({
-            //tidak perlu headers:{} karena sudah di interceptors
-            params: {
-                page: page.value,
-                limit: limit.value,
-                search: search.value
-            }
-        })
 
-        console.log("response search:", res.data)
-
-        console.log(res.data.data)
-        if(!res.data.success){
-            throw new Error(res.data.message)
-        }
-
-        users.value = res.data.data
-        return users.value
-    }catch(err){
-        error.value = err.message
-    }
-}
-
-const searchUser = async (keywoard) => {
-    try{
-        const res = await useUserStore.setSearch(keywoard)
-    } catch (err){
-        error.value = err.message
-    }
-}
-
-const changePage = async (val) => {
-    page.value = val;
-    loadUsers()
-}
 
 onMounted(async () => {
-    await loadUsers();
+    await userStore.loadUsers()
 })
+
+
+const searchUser = async (keywoard) => {
+    await userStore.setSearch(keywoard)
+}
+
+const changePage = async (page) => {
+    await userStore.setPage(page)
+}
 </script>
 
 <template>
@@ -60,11 +28,26 @@ onMounted(async () => {
         <UserSearch
          @search="searchUser"/>
 
-        <UserTable 
+         <div v-if="userStore.loading">
+            Loading users...
+         </div>
+
+         <div v-else-if="userStore.error">
+            {{ userStore.error }}
+            <button @click="userStore.loadUsers">
+                Retry
+            </button>
+         </div>
+         <div v-else-if="userStore.users.length === 0">
+            Users Not Found
+         </div>
+
+        <UserTable v-else
          :users="users" />
         
         <UserPagination 
-        :page="page"
+        :page="userStore.pagination.page"
+        :total_page="userStore.pagination.totalPages"
         @change="changePage"/>
 
     </div>
