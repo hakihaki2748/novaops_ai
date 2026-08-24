970724629f706cb3ffbd9344fa3c019ebdf6ba46
Haki <qu.haky@gmail.com>
2026-08-24 12:32:42 +0700

    menambahkan totalPages

--- frontend/src/components/userManagement/UserPagination.vue
+++ frontend/src/components/userManagement/UserPagination.vue
@@ -1,6 +1,7 @@
 <script setup>
-const {page} = defineProps({
-    page: {type: Number, default: () => 1 }
+const {page, totalPages} = defineProps({
+    page: {type: Number, default: () => 1 },
+    totalPages: {type: Number, default: () => 1 }
 })
 
 const emit = defineEmits(["change"])
@@ -13,7 +14,9 @@ function changePage (val) {
 
 <template>
     <div class="m-1">
-        <button @click="changePage(page - 1)" class="m-1 bg-blue-400 text-white p-1 px-2 rounded-sm">prev</button>
-        <button @click="changePage(page + 1)" class="m-1 bg-blue-400 text-white p-1 px-2 rounded-sm">next</button>
+        <button @click="changePage(page - 1)" class="m-1 bg-blue-400 text-white p-1 px-2 rounded-sm"
+                :class="page === 1 ? 'disabled' : '' ">prev</button>
+        <button @click="changePage(page + 1)" class="m-1 bg-blue-400 text-white p-1 px-2 rounded-sm"
+                :class="page === totalPages ? 'disabled' : ''">next</button>
     </div>
 </template>
--- frontend/src/components/userManagement/UserSearch.vue
+++ frontend/src/components/userManagement/UserSearch.vue
@@ -4,14 +4,16 @@ const emit = defineEmits(["search"])
 
 const keyword = ref("")
 
-function inputSearch  () {
+function submitSearch  () {
     emit("search", keyword.value)
 }
 </script>
 
 <template>
     <div class="p-1">
-        <input type="text" v-model="keyword" class="border rounded-xs m-1 focus:outline-amber-200">
-        <button @click="inputSearch()">search</button>
+        <input type="text" v-model="keyword" class="border rounded-xs m-1 focus:outline-amber-200"
+                placeholder="Search User.."
+                @keyup.enter="submitSearch()">
+        <button @click="submitSearch()">search</button>
     </div>
 </template>
