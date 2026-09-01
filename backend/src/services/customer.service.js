d75104b89b3c31b809dda0e035551dd3069f9240
Haki <qu.haky@gmail.com>
2026-09-01 17:59:07 +0700

    perbaikan

--- backend/src/services/customer.service.js
+++ backend/src/services/customer.service.js
@@ -5,7 +5,7 @@ import AppError from "../utils/AppError"
 const createCustomer = async ({name, email, phone}) => {
     //cari email apakah sudah digunakan
     const findEmail = await customerRepository.findCustomerByEmail(email)
-    if(!findEmail || findEmail.length === 0) throw new AppError("Email Sudah Digunakan", 400)
+    if(!findEmail) throw new AppError("Email Sudah Digunakan", 400)
 
     const newCustomer = await customerRepository.createCustomer({name, email, phone})
 
