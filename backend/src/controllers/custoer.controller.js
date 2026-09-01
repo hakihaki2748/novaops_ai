5e716a74d4ae27484cafed7aae0185c1020536e6
Haki <qu.haky@gmail.com>
2026-09-01 18:34:53 +0700

    menambahkan controller

---
+++ backend/src/controllers/custoer.controller.js
@@ -0,0 +1,20 @@
+//import service
+import customerService from "../services/customer.service"
+import apiResponse from "../utils/apiResponse";
+
+const createCustomer = async (req, res, next) => {
+    const { name, email, phone } = req.body;
+
+    try{
+        const result = await customerService.createUser({name, email, phone})
+        return res.status(201).json(
+            apiResponse.success("Data Di Tambahkan", result)
+        )
+    }catch(err){
+        next(err)
+    }
+}
+
+export default {
+    createCustomer,
+}
